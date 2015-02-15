ea6761c96a88680121e65930c7d5f5db0d545674
CD Cabrera <cabrera.code@gmail.com>
2015-02-15 02:22:55 -0500

    CouchDB Initial
    Initial work for CouchDB.

---
+++ spk/couchdb/src/dsm-control.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Package
+PACKAGE="couchdb"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}/bin/"
+
---
+++ spk/couchdb/src/installer.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Package
+PACKAGE="couchdb"
+DNAME="CouchDB"
+
+# Others
+INSTALL_DIR="/usr/local/${PACKAGE}"
+SSS="/var/packages/${PACKAGE}/scripts/start-stop-status"
+PATH="${INSTALL_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
+
